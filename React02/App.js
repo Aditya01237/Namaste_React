810036a958c37a0065c46189a9d3ab40c27162dc
ADITYA PAREEK <80575917+Aditya01237@users.noreply.github.com>
2024-02-17 19:18:09 +0530

    Ignited the App

--- React02/App.js
+++ React02/App.js
@@ -1,11 +1,9 @@
-
-
-
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 const Parent = React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child1" },[
-        React.createElement("h1",{},"I am H1 of child1"),React.createElement("h2",{},"I am H2 of child1")
+        React.createElement("h1",{},"I am Indian"),React.createElement("h2",{},"I am H2 of child1")
     ]),
     React.createElement("div",{id:"child2"},[
         React.createElement("h1",{},"I am H1 of child2"),React.createElement("h2",{},"I am H2 of child2")
@@ -14,13 +12,6 @@ const Parent = React.createElement("div",{id:"parent"},[
         React.createElement("h1",{},"I am H1 of child3"),React.createElement("h2",{},"I am H2 of child3")
     ])
 ])
-
-
-
-
-
-
-
 // const heading = React.createElement("h1",{id:"heading"},"Hello World from React!");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
